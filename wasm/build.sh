ffab1cf1e5ea97823266b6580eea5b058f2237b0
Velda <68994918+velda228@users.noreply.github.com>
2025-06-18 00:59:17 +0300

    Update build.sh

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd src
-emcc main.c gumbo/src/*.c cJSON/*.c -I./gumbo/src -I./cJSON -s WASM=1 -s EXPORTED_FUNCTIONS='["_get_manga_details"]' -o ../manga_parser.js 
+emcc main.c gumbo/src/*.c cJSON/*.c -I./gumbo/src -I./cJSON -s WASM=1 -s EXPORTED_FUNCTIONS='["_get_manga_list", "_get_manga_details", "_get_chapter_pages"]' -o ../manga_parser.js 
