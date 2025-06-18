fe5364c3736f19dc3fcebcdc88e806b21ec33b13
Velda <68994918+velda228@users.noreply.github.com>
2025-06-18 13:27:37 +0300

    Update build.sh

--- extensions/asurascans/build.sh
+++ extensions/asurascans/build.sh
@@ -6,14 +6,14 @@
 set -e
 
 # Пути к файлам
-WASM_SRC_DIR="../../mangaplatgh/wasm/src"
+WASM_SRC="../../wasm/src"
 PARSER_C="parser.c"
 OUTPUT_DIR="."
 OUTPUT_NAME="asurascans_parser.wasm"
 
 # Проверяем наличие необходимых файлов
-if [ ! -f "$WASM_SRC_DIR/main.c" ]; then
-    echo "Ошибка: Файл $WASM_SRC_DIR/main.c не найден"
+if [ ! -f "$WASM_SRC/main.c" ]; then
+    echo "Ошибка: Файл $WASM_SRC/main.c не найден"
     exit 1
 fi
 
@@ -25,32 +25,21 @@ fi
 echo "Сборка WASM парсера для AsuraScans..."
 
 # Компилируем WASM модуль с библиотеками Gumbo и cJSON
-emcc \
-    "$WASM_SRC_DIR/main.c" \
-    "$PARSER_C" \
-    -I"$WASM_SRC_DIR/gumbo/src" \
-    -I"$WASM_SRC_DIR/cJSON" \
-    "$WASM_SRC_DIR/gumbo/src/attribute.c" \
-    "$WASM_SRC_DIR/gumbo/src/char_ref.c" \
-    "$WASM_SRC_DIR/gumbo/src/error.c" \
-    "$WASM_SRC_DIR/gumbo/src/parser.c" \
-    "$WASM_SRC_DIR/gumbo/src/string_buffer.c" \
-    "$WASM_SRC_DIR/gumbo/src/string_piece.c" \
-    "$WASM_SRC_DIR/gumbo/src/tag.c" \
-    "$WASM_SRC_DIR/gumbo/src/tokenizer.c" \
-    "$WASM_SRC_DIR/gumbo/src/utf8.c" \
-    "$WASM_SRC_DIR/gumbo/src/util.c" \
-    "$WASM_SRC_DIR/gumbo/src/vector.c" \
-    "$WASM_SRC_DIR/cJSON/cJSON.c" \
-    -o "$OUTPUT_DIR/$OUTPUT_NAME" \
-    -s WASM=1 \
-    -s EXPORTED_FUNCTIONS='["_malloc","_free","_get_manga_list","_get_manga_details","_get_chapter_list","_get_chapter_pages","_cleanup","_get_manga_list_asura","_get_manga_details_asura","_get_chapter_list_asura","_get_chapter_pages_asura","_cleanup_asura"]' \
-    -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
-    -s ALLOW_MEMORY_GROWTH=1 \
-    -s INITIAL_MEMORY=65536 \
-    -s MAXIMUM_MEMORY=16777216 \
-    -O2 \
-    --no-entry
+emcc -O3 \
+  -I "$WASM_SRC/cJSON" \
+  -I "$WASM_SRC/gumbo/src" \
+  "$WASM_SRC/cJSON/cJSON.c" \
+  "$WASM_SRC/gumbo/src/"*.c \
+  "$PARSER_C" \
+  -o "$OUTPUT_DIR/$OUTPUT_NAME" \
+  -s WASM=1 \
+  -s EXPORTED_FUNCTIONS='["_get_manga_list_asura","_get_manga_details_asura","_get_chapter_list_asura","_get_chapter_pages_asura","_cleanup_asura"]' \
+  -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s INITIAL_MEMORY=65536 \
+  -s MAXIMUM_MEMORY=16777216 \
+  -O2 \
+  --no-entry
 
 echo "Сборка завершена: $OUTPUT_DIR/$OUTPUT_NAME"
 
